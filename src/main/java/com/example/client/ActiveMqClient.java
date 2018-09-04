e8ba80922db49a93aa3bca5e2cbea81d91deb3f9
Administrator <Administrator@SC-201611261115>
2018-09-04 20:47:00 +0800

    修改

--- src/main/java/com/example/client/ActiveMqClient.java
+++ src/main/java/com/example/client/ActiveMqClient.java
@@ -26,7 +26,7 @@ public class ActiveMqClient {
     	
     	
     	System.out.println("ddddddddddddd");
-        jmsTemplate.convertAndSend("topic","发送的topic数据!");
+        jmsTemplate.convertAndSend("topic","发送的topic!");
         System.out.println("ddddddddddddd");
         System.out.println("ddddddddddddd");
         jmsTemplate.convertAndSend("queue","发送的queue数据!");
